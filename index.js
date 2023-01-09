6813e4d554e3f2d3559dcf3faedc018f093fc66e
Sarina Li <74844953+sarinali@users.noreply.github.com>
2023-01-08 22:37:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-const express = require ("express");
+const express = require("express");
 const mysql = require("mysql");
 const app = express();
 
-app.use(express. json ()) ;
+app.use(express.json());
 const port = process.env.PORT || 8080;
 
-app.listen (port, () => {
-console. log (`Lasyntex Rest API listening on port ${port}`);
+app.listen(port, () => {
+  console.log(`Lasyntex Rest API listening on port ${port}`);
 });
 
-app.get ("/", async (req, res) => {
-res.json ({ status:
-"Lasyntex writing proofs since 2023" });
+app.get("/", async (req, res) => {
+  res.json({status: "Lasyntex writing proofs since 2023" });
+});
+
+app.get("/:mathcommands", async (req, res) => {
+  const query = "SELECT * FROM breeds WHERE name = ?";
+  pool.query(query, [req.params.mathcommands], (error, results) => {
+    if (!results[0]) {
+      res.json({ status: "Not found!" });
+    } else {
+      res.json(results[0]);
+    }
+  });
 });
