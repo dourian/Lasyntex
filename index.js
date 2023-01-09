2ecc489f9d838c6fecad95d035f0e8f6e9fa5fc3
Sarina Li <74844953+sarinali@users.noreply.github.com>
2023-01-08 22:22:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ app.use(express. json ()) ;
 const port = process.env.PORT || 8080;
 
 app.listen (port, () => {
-console. log ('Lasyntex Rest API listening on port ${port}');
+console. log (`Lasyntex Rest API listening on port ${port}`);
 });
 
 app.get ("/", async (req, res) => {
-res.ison ({ status:
+res.json ({ status:
 "Lasyntex writing proofs since 2023" });
 });
