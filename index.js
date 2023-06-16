81c74c0d9e244c44382e82de9c0d5b7a8666ad15
Sarina Li <74844953+sarinali@users.noreply.github.com>
2023-06-15 22:54:02 -0400

    pool connection instead of connection

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const dbUrlObj = new URL(dbUrl);
 console.log(dbUrlObj.host)
 console.log(dbUrlObj.username)
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: dbUrlObj.host,
   user: dbUrlObj.username,
   password: dbUrlObj.password,
@@ -18,13 +18,34 @@ const db = mysql.createConnection({
   reconnectInterval: 2000, // Interval between reconnection attempts (in milliseconds)
 });
 
-db.connect((err) => {
+db.getConnection((err) => {
   if (err) {
     throw err;
   } else {
     console.log("MySql connected");
   }
 })
+
+// var connection;
+// function handleDisconnect() {
+//     connection = mysql.createConnection(db);  // Recreate the connection, since the old one cannot be reused.
+//     connection.connect( function onConnect(err) {   // The server is either down
+//         if (err) {                                  // or restarting (takes a while sometimes).
+//             console.log('error when connecting to db:', err);
+//             setTimeout(handleDisconnect, 10000);    // We introduce a delay before attempting to reconnect,
+//         }                                           // to avoid a hot loop, and to allow our node script to
+//     });                                             // process asynchronous requests in the meantime.
+//                                                     // If you're also serving http, display a 503 error.
+//     connection.on('error', function onError(err) {
+//         console.log('db error', err);
+//         if (err.code == 'PROTOCOL_CONNECTION_LOST') {   // Connection to the MySQL server is usually
+//             handleDisconnect();                         // lost due to either server restart, or a
+//         } else {                                        // connnection idle timeout (the wait_timeout
+//             throw err;                                  // server variable configures this)
+//         }
+//     });
+// }
+// handleDisconnect();
 
 const app = express();
 
