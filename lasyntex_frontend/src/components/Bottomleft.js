2e06381a17fe69ca790cb844c5f9485b5581b98d
dourian <dorcwork@gmail.com>
2023-08-25 21:02:48 -0400

    fix link

--- lasyntex_frontend/src/components/Bottomleft.js
+++ lasyntex_frontend/src/components/Bottomleft.js
@@ -6,7 +6,7 @@ function Bottomleft({ order }) {
       <div className="bottom_left">
         <a href="https://github.com/dourian/Lasyntex">Github</a>
         <a href="mailto:dz2chen@uwaterloo.ca">Contact</a>
-        <a href="https://lasyntex-service-e5x5h3x7kq-uc.a.run.app/api-docs/">
+        <a href="https://lasyntex.herokuapp.com">
           API
         </a>
         <a href="https://docs.google.com/forms/d/e/1FAIpQLSfjeYUZMEeJnH-D9fInzDodiPksfRYL5Ghba7UCOCSqGHbC3Q/viewform">
